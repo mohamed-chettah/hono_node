ed62e6fc18cb0a9bb4b2c5c76f9b8e8d2a560bd6
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-06-20 16:52:20 +0200

    ajout try catch marque

--- my-app/src/routes/marques.ts
+++ my-app/src/routes/marques.ts
@@ -5,15 +5,26 @@ import { isValidObjectId } from "mongoose";
 const api = new Hono().basePath("/marques");
 
 api.get("/", async (c) => {
-    const allMarques = await Marque.find({});
-    return c.json(allMarques);
+    try {
+        const allMarques = await Marque.find({});
+        return c.json(allMarques);
+    } catch (error) {
+        return c.json({ msg: "An error occurred while fetching the marques", error: error.message }, 500);
+    }
 });
 
 api.get("/:marqueId", async (c) => {
     const _id = c.req.param("marqueId");
     if (isValidObjectId(_id)) {
-        const oneMarque = await Marque.findOne({ _id });
-        return c.json(oneMarque);
+        try {
+            const oneMarque = await Marque.findOne({ _id });
+            if (oneMarque) {
+                return c.json(oneMarque);
+            }
+            return c.json({ msg: "Marque not found" }, 404);
+        } catch (error) {
+            return c.json({ msg: "An error occurred while fetching the marque", error: error.message }, 500);
+        }
     }
     return c.json({ msg: "ObjectId malformed" }, 400);
 });
@@ -25,7 +36,7 @@ api.post("/", async (c) => {
         const saveMarque = await newMarque.save();
         return c.json(saveMarque, 201);
     } catch (error: any) {
-        return c.json(error._message, 400);
+        return c.json({ msg: "An error occurred while saving the marque", error: error._message }, 400);
     }
 });
 
@@ -37,10 +48,17 @@ api.put("/:marqueId", async (c) => {
     }
     const q = { _id };
     const updateQuery = { ...body };
-    const tryToUpdate = await Marque.findOneAndUpdate(q, updateQuery, {
-        new: true,
-    });
-    return c.json(tryToUpdate, 200);
+    try {
+        const tryToUpdate = await Marque.findOneAndUpdate(q, updateQuery, {
+            new: true,
+        });
+        if (tryToUpdate) {
+            return c.json(tryToUpdate, 200);
+        }
+        return c.json({ msg: "Marque not found" }, 404);
+    } catch (error) {
+        return c.json({ msg: "An error occurred while updating the marque", error: error.message }, 500);
+    }
 });
 
 api.patch("/:marqueId", async (c) => {
@@ -51,10 +69,17 @@ api.patch("/:marqueId", async (c) => {
     }
     const q = { _id };
     const updateQuery = { $set: { ...body } };
-    const tryToUpdate = await Marque.findOneAndUpdate(q, updateQuery, {
-        new: true,
-    });
-    return c.json(tryToUpdate, 200);
+    try {
+        const tryToUpdate = await Marque.findOneAndUpdate(q, updateQuery, {
+            new: true,
+        });
+        if (tryToUpdate) {
+            return c.json(tryToUpdate, 200);
+        }
+        return c.json({ msg: "Marque not found" }, 404);
+    } catch (error) {
+        return c.json({ msg: "An error occurred while updating the marque", error: error.message }, 500);
+    }
 });
 
 api.delete("/:marqueId", async (c) => {
@@ -62,12 +87,16 @@ api.delete("/:marqueId", async (c) => {
     if (!isValidObjectId(_id)) {
         return c.json({ msg: "ObjectId malformed" }, 400);
     }
-    const tryToDelete = await Marque.deleteOne({ _id });
-    const { deletedCount } = tryToDelete;
-    if (deletedCount) {
-        return c.json({ msg: "DELETE done" });
+    try {
+        const tryToDelete = await Marque.deleteOne({ _id });
+        const { deletedCount } = tryToDelete;
+        if (deletedCount) {
+            return c.json({ msg: "DELETE done" });
+        }
+        return c.json({ msg: "Marque not found" }, 404);
+    } catch (error) {
+        return c.json({ msg: "An error occurred while deleting the marque", error: error.message }, 500);
     }
-    return c.json({ msg: "not found" }, 404);
 });
 
 export default api;
